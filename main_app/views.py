c1978f2dbaa5a185d279a746e8bc4d3b846b6fe5
Daniel Werminghausen <daniel.werminghausen.com>
2021-11-18 20:10:01 -0500

    wip: template proj update

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,24 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def upcomingtrips_index(request):
+  return render(request, 'trips/index.html', { 'trips': trips })
+
+
+class Trip:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, First_name, Last_name, vaccinated, location, date, Past_trip, images, review):
+    self.First_name = First_name
+    self.Last_name = Last_name
+    self.vaccinated = vaccinated
+    self.location = location
+    self.date = date
+    self.Past_trip = Past_trip
+    self.images = images
+    self.review = review
+
+trips = [
+  Trip('Dan', 'Werm', True, 'Germany', 'Nov-12-2022', False, None, None),
+#   Trip('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#   Trip('Raven', 'black tripod', '3 legged cat', 4)
+]
