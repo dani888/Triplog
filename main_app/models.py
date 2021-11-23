acb73c1a926536ce052cfe8099321b5602ba5789
Daniel Werminghausen <daniel.werminghausen.com>
2021-11-23 11:33:14 -0500

    Photo feature working using DW AWS

--- main_app/models.py
+++ main_app/models.py
@@ -19,3 +19,10 @@ class Trip(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'trip_id': self.id})
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for trip_id: {self.trip_id} @{self.url}"
