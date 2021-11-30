ee7fa11f4af3ca37506f2dda0c19cfaf6e5fecf8
mariorecinos <recinos.mario68@gmail.com>
2021-11-30 12:04:02 -0800

    added user to comment

--- main_app/forms.py
+++ main_app/forms.py
@@ -5,3 +5,9 @@ class CommentForm(ModelForm):
   class Meta:
     model = Comment
     fields = ['date', 'comment']
+
+    def form_valid(self, form):
+      # Assign the logged in user (self.request.user)
+      form.instance.user = self.request.user  # form.instance is the cat
+      # Let the CreateView do its job as usual
+      return super().form_valid(form)
